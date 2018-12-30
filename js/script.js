4225b0262e7bd621b794056958d716a1ee6c7f27
ShevRuslan <sasa02sasa@mail.ru>
2018-12-30 16:14:18 +0600

    Update function validate input

--- js/script.js
+++ js/script.js
@@ -9,30 +9,31 @@ buttonSubmit.addEventListener('click', function(e) {
         if(elem.type == 'submit') {
             return;
         }
-        arraySend[elem.name] = elem.value;
+        arraySend.push(elem);
     });
-    let formValidate  = new FormValidator(arraySend, null);
+    let formValidate  = new FormValidator(arraySend);
 })
 
 class FormValidator {
-    constructor(elements, validator) {
+    constructor(elements) {
         this.elements = elements;
-        this.functionValidator = validator;
         this.init();
     }
-    check() {
-        for(let key in this.elements) {
-            let element = document.querySelector('#' + key);
-
-            if(this.elements[key] == ""){
-                console.log(key);
-                element.classList.add('border__red');
-                continue;
-            }
+    validateAllInputs() {
+        // this.elements.forEach(function(elem) {
+        //     this.validateOneInput(elem);
+        // }.bind(this));
+        this.elements.forEach(element => this.validateOneInput(element) );
+    }
+    validateOneInput(element) {
+        if(element.value.trim() == ""){
+            element.classList.add('border__red');
+        }
+        else {
             element.classList.remove('border__red');
         }
     }
     init() {
-        this.check();
+        this.validateAllInputs();
     }
 }
